7018d735d3c9275000d8d264b307cdf6d18bc1c1
mgh3326 <mgh3326@naver.com>
2018-12-10 08:37:40 +0900

    first update

---
+++ license/resource.h
@@ -0,0 +1,35 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// license.rc에서 사용되고 있습니다.
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_LICENSE_DIALOG              102
+#define IDR_MAINFRAME                   128
+#define IDD_DIALOG1                     129
+#define IDR_MENU1                       130
+#define IDR_RT_MANIFEST1                132
+#define IDR_241                         133
+#define IDR_242                         134
+#define IDR_243                         135
+#define IDC_PICTURE                     1000
+#define IDC_Exact                       1001
+#define IDC_Mosaic                      1002
+#define IDC_Open                        1003
+#define ID_FILE_FILEOPEN                32771
+#define ID_FILE_FILESAVE                32772
+#define ID_FILE_FILECLOSE               32773
+#define ID_EDIT_FINDCONTOUR             32774
+#define ID_EDIT_MOSIAC                  32775
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        136
+#define _APS_NEXT_COMMAND_VALUE         32776
+#define _APS_NEXT_CONTROL_VALUE         1004
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
